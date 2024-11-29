a4f233d7f94ffe577d06fa792e7857ae11b192f5
Osmely Fernandez <osmely@kinesis.co.il>
2024-11-29 12:08:34 -0500

    Add Android logging import to FlutterAmazonAdmPlugin

--- android/src/main/java/com/kinesis/flutter_amazon_adm/FlutterAmazonAdmPlugin.java
+++ android/src/main/java/com/kinesis/flutter_amazon_adm/FlutterAmazonAdmPlugin.java
@@ -15,6 +15,8 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import android.util.Log;
+
 
 public class FlutterAmazonAdmPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
     private static MethodChannel methodChannel;
